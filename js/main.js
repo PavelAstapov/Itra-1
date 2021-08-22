2c0a3684c87b494ec250dcfd37727d55a44509c4
PavelAstapov <astapov1991@gmail.com>
2021-08-22 15:04:58 +0300

    name

--- js/main.js
+++ js/main.js
@@ -35,25 +35,52 @@ document.addEventListener("DOMContentLoaded", function(event) {
         $('#coffee_catalog_row .coffee_catalog_row_box .coffee_catalog_row_slider .slick-cloned:nth-child(2)').css({
             "opacity": "1"
         })
-        
       });
 
     $('.coffee_catalog_row_slider').on('afterChange', function(event, slick, currentSlide){
         $('#coffee_catalog_row .coffee_catalog_row_box .coffee_catalog_row_slider .slick-slide').prev('.slick-slide').css({
             'opacity': "0.5"
-    });
-    $('#coffee_catalog_row .coffee_catalog_row_box .coffee_catalog_row_slider .slick-center.slick-active').prev('.slick-slide').css({
-            'opacity': "1"
-    });
-
-    $('#coffee_catalog_row .coffee_catalog_row_box .coffee_catalog_row_slider .slick-center.slick-active').css({
-            'opacity': "1"
         });
-    $('#coffee_catalog_row .coffee_catalog_row_box .coffee_catalog_row_slider .prev').css({
+        $('#coffee_catalog_row .coffee_catalog_row_box .coffee_catalog_row_slider .slick-center.slick-active').prev('.slick-slide').css({
+                'opacity': "1"
+        });
+
+        $('#coffee_catalog_row .coffee_catalog_row_box .coffee_catalog_row_slider .slick-center.slick-active').css({
+                'opacity': "1"
+            });
+        $('#coffee_catalog_row .coffee_catalog_row_box .coffee_catalog_row_slider .prev').css({
+            "visibility" : "visible"
+        })
+    });
+    
+    document.getElementById("defaultOpen").click();
+
+    $('.coffee_catalog_columns_slider').slick({
+      centerMode: true,
+      slidesToShow: 3,
+      centerPadding: '120px',
+      draggable: false,
+      prevArrow: '<div class="prev"></div>',
+      nextArrow: '<div class="next"></div>',
+      // infinite: false,
+    });
+    $('.coffee_catalog_columns_slider').on('afterChange', function(event, slick, currentSlide){
+      $('#coffee_catalog_columns .coffee_catalog_columns_slider .prev').css({
         "visibility" : "visible"
     })
+    $('#coffee_catalog_columns .coffee_catalog_columns_slider .slick-cloned:nth-child(3)').css({
+      "visibility" : "visible"
+  })
+
     });
-    document.getElementById("defaultOpen").click();
+
+    $('.coffee_catalog_row_slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+      $('#coffee_catalog_row .coffee_catalog_row_box .coffee_catalog_row_slider .slick-cloned:nth-child(2)').css({
+          "opacity": "1"
+      })
+    });
+
+
     
   });
 
@@ -70,7 +97,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+      tablinks[i].className = tablinks[i].className.replace("active", "");
     }
   
     // Show the current tab, and add an "active" class to the button that opened the tab
